6f8463f64aa8350691744eacddb1876a331e0f57
chakri3130 <amar.chakri426@gmail.com>
2021-02-06 20:22:55 +0530

    updated with latest code

--- src/ios/Integration/livepersonswift.swift
+++ src/ios/Integration/livepersonswift.swift
@@ -1,18 +1,48 @@
 @objc(livepersonswift) class livepersonswift : CDVPlugin{
-
+    typealias LPSDKCompletion = (Bool) -> Void
 var pluginResult = CDVPluginResult(status: CDVCommandStatus_ERROR)
+    var LPwrapper:LPMessagingWrapper?
+//    var engagement:LPGetEngagementResponse?
 @objc(add:) func add(_ command: CDVInvokedUrlCommand) {
 var pluginResult = CDVPluginResult(status: CDVCommandStatus_ERROR)
-let param1 = (command.arguments[0]) as? Int
-let param2 = (command.arguments[0]) as? Int
-if let p1 = param1 , let p2 = param2 {
-if p1 >= 0 && p1 >= 0{
- let total = String(p1 + p2)
-pluginResult = CDVPluginResult(status: CDVCommandStatus_OK, messageAs: total)
-}else {
-pluginResult = CDVPluginResult(status: CDVCommandStatus_ERROR, messageAs: "Something wrong")
-}
-}
+    if (self.LPwrapper == nil) {
+    LPwrapper = LPMessagingWrapper.init(user: nil, authenticationParams: nil)
+    pluginResult = CDVPluginResult(status: CDVCommandStatus_OK, messageAs: "Messaging wrapper has been initialized successfully")
+    }
+    else
+    {
+        ConnectToBot(command)
+    }
 self.commandDelegate!.send(pluginResult, callbackId: command.callbackId)
 }
+
+    @objc(ConnectToBot:) func ConnectToBot(_ command: CDVInvokedUrlCommand) {
+        
+//        let engagementCountry = command.arguments[0]  as! String ?? "iOS-default"
+//        var engagementStep = command.arguments[1] as! String ?? "dev"
+//        var engagmentLng = command.arguments[2] as! String ?? "us"
+//        var engagementEnv = command.arguments[3] as! String ?? "es"
+        let customEntryPoints = ["ios-tutorial-step1","dev","us","es"]
+        self.LPwrapper?.getEngagement(entryPoints: customEntryPoints) { (result) in
+            if(result == true)
+            {
+            self.LPwrapper?.showChat(completion: { (success) in
+                print("success")
+            })
+            }
+        }
+        
+
+        
+    }
+    
+
+//    @objc public func getEngagement(entryPoints: [String],
+//                                    completion: ((Bool) -> Void)?) {
+//
+//
+//    }
+    
+    
+    
 }
